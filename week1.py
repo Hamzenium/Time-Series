5223b1ee884cb0ea0009e66378854a98cdf4a3eb
Muhammad Hamza Sohail <muhammadhamzasohail@Muhammads-MacBook-Air.local>
2023-05-17 05:00:06 +0300

    Add all my files

---
+++ week1.py
@@ -0,0 +1,37 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+import matplotlib.pyplot as plt
+
+def trend(time, slope=0):
+    """A trend over time"""
+    return slope * time
+
+def seasonal_pattern(season_time):
+    """Just an arbitrary pattern"""
+    return np.where(season_time < 0.1,
+                    np.cos(season_time * 7 * np.pi),
+                    1 / np.exp(5 * season_time))
+
+
+def seasonality(time, period, amplitude=1, phase=0):
+    """Repeats the same pattern at each period"""
+    season_time = ((time + phase) % period) / period
+    return amplitude * seasonal_pattern(season_time)
+
+
+def noise(time, noise_level=1, seed=None):
+    """Adds noise to the series"""
+    rnd = np.random.RandomState(seed)
+    return rnd.randn(len(time)) * noise_level
+
+
+def plot_series(time, series, format="-", title="", label=None, start=0, end=None):
+    """Plot the series"""
+    plt.plot(time[start:end], series[start:end], format, label=label)
+    plt.xlabel("Time")
+    plt.ylabel("Value")
+    plt.title(title)
+    if label:
+        plt.legend()
+    plt.grid(True)
