71df2f210252c34dc41e0f3873b24b38a8fe25b7
Muhammad Hamza Sohail <muhammadhamzasohail@Muhammads-MacBook-Air.local>
2023-05-24 00:04:12 -0400

    Single Neural Network

---
+++ single_neural_network.py
@@ -0,0 +1,43 @@
+import tensorflow as tf
+import numpy as np
+
+
+window_size = 20
+batch_size =32
+shuffle_buffer_size = 1000
+x_train = []
+
+# first we have to divide our data into features or the x_labels and labels
+# the feature will be the number of the values in the series with our label being the next value
+# we take a window of the dataset we have and train the model using the
+
+def windowed_dataset(series, window_size, batch_size, shuffle_buffer):
+    #first it generates a tf dataset from time series values.
+    dataset = tf.data.Dataset.from_tensor_slices(series)
+
+    #will take the dataset,with a specific windowsize, drops others
+
+    dataset = dataset.window(window_size + 1,shift=1, drop_remainder=True)
+
+    #flattens the windowset by putting its element in a signle  batch
+    dataset = dataset.flat_map(lambda window: window.batch(window_size + 1))
+
+    # creates tuples with features and labells
+    dataset = dataset.shuffle(shuffle_buffer).map(lambda window :(window[:-1],window[-1]))
+
+    #shuffles windows
+    
+    dataset  = dataset.batch(batch_size).prefetch(1)
+    return dataset
+
+
+dataset = windowed_dataset(x_train, window_size, batch_size, shuffle_buffer_size)
+series = []
+
+l0 = tf.keras.layers.Dense(1, input_shape = [window_size])
+model = tf.keras.models.Sequential[10]
+
+model.compile(loss='mse', optimizer=tf.keras.optimizers.SGD(learning_rate=1e-6, momentum=0.9))
+
+model.finalize_state(dataset,epochs = 100)
+model.predict(series[0:20][np.axsis])
